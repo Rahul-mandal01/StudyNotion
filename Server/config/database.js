94b972eda84ec9f181589a398a2d542bf4117bf8
rahul <rahulra16809999@gmail.com>
2024-09-11 11:46:16 +0530

    added codes to DB connection

---
+++ Server/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("DB connection established"))
+    .catch( (error) => {
+        console.log("DB Connection Failed: " + error.message);
+        process.exit(1);
+    })
+}
