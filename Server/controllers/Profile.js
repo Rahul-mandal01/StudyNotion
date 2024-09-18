e943ce275377fba516f72937f654deb00d379d3f
rahul <rahulra16809999@gmail.com>
2024-09-18 11:03:04 +0530

    Added deleteAccount & getAllUserDetails handler function

--- Server/controllers/Profile.js
+++ Server/controllers/Profile.js
@@ -51,3 +51,78 @@ exports.updateProfile = async (req, res) => {
 };
 
 // ACCOUNT DELETE
+
+exports.deleteAccount = async (req, res) =>{
+    try{
+        // GET USER ID
+        const id = req.user.id;
+
+        // VALIDATION
+        const userDetails = await User.findById(id);
+        if(!userDetails){
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+
+        // DELETE PROFILE
+        await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
+
+
+
+        // HOMEWORK-----------------> UNEROLL USER FROM ALL ENROLLED COURSES--------
+
+
+
+        // DELETE USER
+        await User.findByIdAndDelete({_id: id});
+
+        
+        // RETURN RESPONSE
+        return res.status(200).json({
+            success: true,
+            message: "Account deleted successfully",
+        });
+
+
+    }catch(error){
+        console.log("Error deleting account: " + error);
+        return res.status(500).json({
+            success: false,
+            message: `An error occurred: ${error.message}`,
+        })
+    }
+}
+
+
+// FETCH ALL DETAILS OF USER
+
+exports.getAllUserDetails = async (req, res) => {
+    try{
+        // GET USER ID
+        const id = req.user.id
+
+        // GET USER DETAIL AND VALIDATE USER
+        const userDetails = await User.findById(id).populate("additionalDetails").exec();
+        if(!userDetails){
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+
+        // RETURN RESPONSE
+        return res.status(200).json({
+            success: true,
+            message: "User details fetched successfully",
+            userDetails: userDetails,
+        })
+        
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: `An error occurred: ${error.message}`,
+        })
+    }
+}
