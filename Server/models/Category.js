d53e4861946adbd6ad73e09b8fc12a6282de416f
rahul <rahulra16809999@gmail.com>
2024-09-29 09:39:33 +0530

    debug code

--- Server/models/Category.js
+++ Server/models/Category.js
@@ -11,10 +11,12 @@ const categorySchema = new mongoose.Schema({
         type: "string",
     },
 
-    course:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"Course",
-    }
+    courses:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Course",
+        }
+    ]
 })
 
 module.exports = mongoose.model("Tag", categorySchema);
