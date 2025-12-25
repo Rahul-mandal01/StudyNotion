d786c26997c1fba741861d7226b719ea2c09fdef
Rahul-mandal01 <rahulra16809999@gmail.com>
2025-12-25 20:29:57 +0530

    Fix import path for navbar links and tidy routing imports.

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import logo from "../../assets/Logo/Logo-Full-Light.png";
 import { Link, matchPath } from "react-router-dom";
-import { NavbarLinks } from "../../data/navbar-links";
+import { NavbarLinks } from "../../data/navbarLinks";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { AiOutlineShoppingCart } from "react-icons/ai";
